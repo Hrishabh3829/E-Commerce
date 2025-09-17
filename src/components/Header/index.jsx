b088f4e031eab7d3c8674ff8ef22018a278e41e6
Hrishabh3829 <2200032748@kluniversity.in>
2025-09-18 01:14:36 +0530

    setup vite and tailwind

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="top-strip py-2 bg-red-300">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <div className="col1 w-[50%]">
+              <p className="text-[14px] font-bold">
+                Get upto 50% off new season styles,limited time only
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
