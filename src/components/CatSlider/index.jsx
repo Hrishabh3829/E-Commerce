e6d71961bd924bbac8fb868e67031e1ce0e3912f
Hrishabh3829 <2200032748@kluniversity.in>
2025-09-28 19:39:42 +0530

    category slider done

--- src/components/CatSlider/index.jsx
+++ src/components/CatSlider/index.jsx
@@ -11,8 +11,17 @@ const HomeCatSlider = () => {
       <div className="container">
         <Swiper
           slidesPerView={6}
-          spaceBetween={10}
+          spaceBetween={12}
           navigation={true}
+          loop={false}
+          grabCursor={true}
+          breakpoints={{
+            0: { slidesPerView: 2.2, spaceBetween: 10 },
+            480: { slidesPerView: 3.2, spaceBetween: 10 },
+            640: { slidesPerView: 4.2, spaceBetween: 12 },
+            768: { slidesPerView: 5.2, spaceBetween: 12 },
+            1024: { slidesPerView: 6.2, spaceBetween: 12 },
+          }}
           modules={[Navigation]}
           className="mySwiper"
         >
@@ -23,6 +32,114 @@ const HomeCatSlider = () => {
                   <FaTshirt className="text-gray-700" size={36} />
                 </div>
                 <h3 className="text-[15px] font-[500] mt-3">Dress</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/508/508786.png"
+                  alt="Shoes"
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Shoes</h3>
               </div>
             </Link>
           </SwiperSlide>
--- src/components/HomeSlider/index.jsx
+++ src/components/HomeSlider/index.jsx
@@ -1,48 +1,71 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
 
-import { Navigation } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 
 const HomeSlider = () => {
   return (
-    <div className="homeSlider py-4">
-      <div className="container">
-        <Swiper
-          spaceBetween={10}
-          navigation={true}
-          modules={[Navigation]}
-          className="sliderHome"
-        >
-          <SwiperSlide>
-            <div className="item rounded-[10px] overflow-hidden">
-              <img
-                src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/CEPC/Jupiter/hero/desktops/UNREC/PC_3000X1200_Unrec_LIVE_ASIN_Headphones._CB799215974_.jpg"
-                alt="Banner Slide"
-                className="w-full"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item rounded-[10px] overflow-hidden">
-              <img
-                src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/WLA/2025/Jupiter25/GW/ME/UR/D298880126_IN_WLA_Jupiter_Hero_Banners_Unrec_PC_PB_LIVE_ASIN_3000x1200._CB800959465_.jpg"
-                alt="Banner Slide"
-                className="w-full"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item rounded-[10px] overflow-hidden">
-              <img
-                src="https://images-eu.ssl-images-amazon.com/images/G/31/Img25/Consumables/HPC/Jupiter/Phase1/GW/Revised/Revised_Unrec_PC_Hero_3000x1200._CB799263776_.png"
-                alt="Banner Slide"
-                className="w-full"
-              />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
+    <div className="homeSlider px-2 md:px-3">
+      <Swiper
+        spaceBetween={10}
+        centeredSlides={true}
+        grabCursor={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="item rounded-[12px] overflow-hidden h-40 sm:h-56 md:h-72 lg:h-80 xl:h-96">
+            <img
+              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/CEPC/Jupiter/hero/desktops/UNREC/PC_3000X1200_Unrec_LIVE_ASIN_Headphones._CB799215974_.jpg"
+              alt="Banner Slide"
+              className="w-full h-full object-cover block"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item rounded-[12px] overflow-hidden h-40 sm:h-56 md:h-72 lg:h-80 xl:h-96">
+            <img
+              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/WLA/2025/Jupiter25/GW/ME/UR/D298880126_IN_WLA_Jupiter_Hero_Banners_Unrec_PC_PB_LIVE_ASIN_3000x1200._CB800959465_.jpg"
+              alt="Banner Slide"
+              className="w-full h-full object-cover block"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item rounded-[12px] overflow-hidden h-40 sm:h-56 md:h-72 lg:h-80 xl:h-96">
+            <img
+              src="https://rukminim2.flixcart.com/fk-p-flap/3240/540/image/8c2598b5c820a42b.jpg?q=60"
+              alt="Banner Slide"
+              className="w-full h-full object-cover block"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item rounded-[12px] overflow-hidden h-40 sm:h-56 md:h-72 lg:h-80 xl:h-96">
+            <img
+              src="https://rukminim2.flixcart.com/www/2140/1540/promos/15/09/2023/9b08f194-51e5-4e6a-a052-f5f91d75b8a0.jpg?q=60"
+              alt="Banner Slide"
+              className="w-full h-full object-cover block"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item rounded-[12px] overflow-hidden h-40 sm:h-56 md:h-72 lg:h-80 xl:h-96">
+            <img
+              src="https://images-eu.ssl-images-amazon.com/images/G/31/Img25/Consumables/HPC/Jupiter/Phase1/GW/Revised/Revised_Unrec_PC_Hero_3000x1200._CB799263776_.png"
+              alt="Banner Slide"
+              className="w-full h-full object-cover block"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
