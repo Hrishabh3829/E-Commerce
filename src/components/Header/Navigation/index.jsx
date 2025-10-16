62b36dd2880d64ad8d8c367dbe4b1ec460db8965
Hrishabh3829 <2200032748@kluniversity.in>
2025-10-16 23:27:56 +0530

    responsiveness done

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -5,18 +5,20 @@ import { LiaAngleDownSolid } from "react-icons/lia";
 import { RiMenu2Fill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import CategoryPanel from "./CategoryPanel";
+import "./style.css";
 
 const Navigation = () => {
   const [isOpenCatPanel, setIsOpenCatPanel] = useState(false);
   const openCategoryPanel = () => {
     setIsOpenCatPanel(true);
   };
+  
 
   return (
     <>
       <nav>
-        <div className=" container flex items-center justify-end gap-8">
-          <div className="col_1 w-[20%]">
+  <div className=" container flex flex-col lg:flex-row items-center justify-end gap-3 lg:gap-8">
+          <div className="col_1 w-full lg:w-[20%]">
             <Button
               className="!text-black !font-[700] gap-2 w-full"
               onClick={openCategoryPanel}
@@ -26,8 +28,8 @@ const Navigation = () => {
               <LiaAngleDownSolid className="text-[13px] ml-auto font-bold " />
             </Button>
           </div>
-          <div className="col_2 w-[60%]">
-            <ul className="flex items-center gap-4 nav">
+          <div className="col_2 w-full lg:w-[60%] min-w-0">
+            <ul className="flex flex-wrap lg:flex-nowrap items-center justify-center gap-2 lg:gap-4 nav">
               <li className="list-none">
                 <Link to="/" className="link transition text-[14px] font-[700]">
                   <Button className="link transition !font-[700] !text-[rgba(0,0,0,0.8)] hover:!text-[#90ee90] !py-4 ">
@@ -172,15 +174,15 @@ const Navigation = () => {
               <li className="list-none">
                 <Link to="/" className="link transition text-[14px] font-[700]">
                   <Button className="link transition !font-[700] !text-[rgba(0,0,0,0.8)] hover:!text-[#90ee90] !py-4 ">
-                    Jwellery
+                    Jewellery
                   </Button>
                 </Link>
               </li>
             </ul>
           </div>
 
-          <div className="col_3 w-[20%]">
-            <p className="text-[14px] font-[700] flex items-center gap-3 mb-0 mt-0">
+          <div className="col_3 w-full lg:w-[20%] mt-2 lg:mt-0 shrink-0">
+            <p className="text-[13px] lg:text-[14px] font-[700] flex items-center gap-3 mb-0 mt-0 justify-center lg:justify-end whitespace-nowrap">
               <GoRocket className="text-[18px]" />
               Free International Delivery
             </p>
--- src/components/ProductSlider/index.jsx
+++ src/components/ProductSlider/index.jsx
@@ -13,6 +13,14 @@ const ProductSlider = (props) => {
         navigation={true}
         loop={false}
         grabCursor={true}
+        breakpoints={{
+          0: { slidesPerView: 2, spaceBetween: 10 },
+          480: { slidesPerView: 2.3, spaceBetween: 12 },
+          640: { slidesPerView: 3, spaceBetween: 12 },
+          768: { slidesPerView: 4, spaceBetween: 14 },
+          1024: { slidesPerView: Math.min(props.items || 6, 5), spaceBetween: 16 },
+          1280: { slidesPerView: props.items || 6, spaceBetween: 18 },
+        }}
         modules={[Navigation]}
         className="mySwiper"
       >
